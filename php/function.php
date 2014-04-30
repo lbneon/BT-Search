a3cdae86dd2e60779b6ca93f4cb794d86d610121
lbneon <lbneon@vm-1.lbneon.koding.kd.io>
2014-04-30 01:47:04 +0000

    update

--- php/function.php
+++ php/function.php
@@ -96,7 +96,7 @@ function Collection($keyword, $page) {
 			$bt['url'] = "magnet:".$magnet_infos[$i]['1'];
 			$bt_json[$i] =$bt;
 		}
-		return count($list['0']);
+		return count($list);
 		return $bt_json;
 	} else {
 		return false;

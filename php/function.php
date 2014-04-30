65204c13c050660ac293ffbbb014684fb3e2608f
lbneon <lbneon@gmail.com>
2014-04-30 19:37:46 +0000

    update

--- php/function.php
+++ php/function.php
@@ -52,7 +52,6 @@ function Curl_content($keyword, $page = '') {
 		$curl = new cURL();
 		$url = 'http://torrentkitty.org/search/';
 		$content = $curl->get($url.$keyword.$page);
-		return $url.$keyword.$page;
 		$cache->set($keyword.$page, $content, 2592000);
 		$content = $curl->get("http://torrentkitty.org/search/%E6%9C%BA%E5%99%A8/");
 		return $content;

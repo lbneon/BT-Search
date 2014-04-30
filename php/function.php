cd1cba255d1765694c4e96d2def78ee6686e1702
lbneon <lbneon@gmail.com>
2014-04-30 21:02:38 +0000

    update

--- php/function.php
+++ php/function.php
@@ -54,8 +54,6 @@ function Curl_content($keyword, $page = '') {
 		$url = 'http://torrentkitty.org/search/%E6%9C%BA%E5%99%A8/';
 		$url = 'http://www.baidu.com';
 		#$content = $curl->get(urlencode($url.$keyword.$page));
-		$content = $curl->get($url);
-		$curl = new cURL();
 		$url = 'http://torrentkitty.org/search/%E6%9C%BA%E5%99%A8/';
 		$content = $curl->get($url);
 		$cache->set($keyword.$page, $content, 2592000);

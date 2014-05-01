0f7da23056d393362de8a0e1859661e68a4f7a44
lbneon <lbneon@gmail.com>
2014-05-01 19:07:37 +0000

    update

--- php/function.php
+++ php/function.php
@@ -112,10 +112,11 @@ function Collection($keyword, $page) {
 			$bt = array();
 			preg_match_all("/<a(.[^>]*)>(.+?)<\/a>/ms", $video_info[0][2][0], $video_info_title[]);
 			$bt['name'] = $video_info_title[0][2][0];
-			preg_match_all("/<span>(.+?)<\/span>/ms", $video_info[0][2][2], $video_info_detail[]);
+			preg_match_all("/<span>(.+?)<\/span>/ms", $video_info[0][2][2], $video_info_detail);
+			return $video_info_detail;
 			$bt['size'] = $video_info_detail[0][2][1];
 			$bt['date'] = $video_info_detail[0][2][0];
-			$bt['url'] = "magnet:".$magnet_infos[0][0][0];
+			$bt['url'] = "magnet:".$magnet_infos[0][2][0];
 			$bt_json[$i] =$bt;
 		}
 		

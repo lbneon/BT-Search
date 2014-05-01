be02910d82f2e1da7c46942255d4215aa4d9428d
lbneon <lbneon@gmail.com>
2014-05-01 18:31:01 +0000

    update

--- php/function.php
+++ php/function.php
@@ -108,7 +108,7 @@ function Collection($keyword, $page) {
 		for ($i=0; $i < count($list['0']); $i++) {
 			$video_list = $list['0'];
 			preg_match_all("/<div(.[^>]*)>(.+?)<\/div>/ms", $video_list[$i], $video_info[]);
-			preg_match ("/href=\"magnet:(.+?)\"/ms", $video_info[$i]['2']['2'], $magnet_infos[]);
+			preg_match ("/href=\"magnet:(.+?)\"/ms", $video_info['0']['2'], $magnet_infos[]);
 			$bt = array();
 			preg_match_all("/<a(.[^>]*)>(.+?)<\/a>/ms", $video_info[$i]['2']['0'], $video_info_title[]);
 			$bt['name'] = $video_info_title[$i]['2']['0']; //$video_info[$i]['2']['0'];

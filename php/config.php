227854d4448f2ce2c351312dbb3a5042988a30cc
lbneon <lbneon@gmail.com>
2014-06-02 14:50:26 +0000

    update

--- php/config.php
+++ php/config.php
@@ -1,36 +1,57 @@
 <?php
+/**
+ * config.php
+ *
+ * 应用程序的配置文件
+ *
+ * @author     Kslr
+ * @copyright  2014 kslrwang@gmail.com
+ * @version    0.3
+ */
+
 define('IN_SYS', TRUE);
-// ========== 数据库配置 ==========
+define("APP_ROOT", dirname(__FILE__));
 
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+/* 
+*   网站信息
+*/
+$siteconf = array(
 
-$dbhost = constant("DB_HOST"); // Host name 
-$dbport = constant("DB_PORT"); // Host port
-$dbusername = constant("DB_USER"); // Mysql username 
-$dbpassword = constant("DB_PASS"); // Mysql password 
-$db_name = constant("DB_NAME"); // Database name 
+    'url'          		=> 'http://www.bunny.cf/',	//网站地址  注意,网址后面必须加上/
+    'title'  	   		=> '兔子 搜搜 - 专业磁力搜索工具,下载利器,免费下载各种资源',	//网站标题
+    'keywords'    		=> '兔子搜素,磁力种子,磁力搜索引擎,磁力搜索,bt搜索器,bt搜索神器,,bt种子搜索器',	//网站关键词
+    'description'  		=> '兔子 搜搜 -- 专注于提供磁力搜索和下载服务,你可以在这里搜索和下载电影、剧集、音乐、图书、图片、综艺、软件、动漫、教程、游戏等资源。',	//网站描述
+    'badword'      		=> array('胡锦涛', '江泽民', '邓小平', '毛泽东'),	//关键词黑名单
+    'default_keyword'   => '无人区',	//默认的关键词
 
+);
 
-$GLOBALS['DB'] = array(
-	'database_type' => 'mysql',
-	'database_name' => $db_name,
-	'server' => $dbhost,
-	'username' => $dbusername,
-	'password' => $dbpassword,
-	'port' => $dbport,
-	'charset' => 'utf8',
-	'option' => array(PDO::ATTR_CASE => PDO::CASE_NATURAL)
-	);
+#=======================================================
+########  如果没有需要，请不要更改下面的配置  ##########
+#=======================================================
 
-$SITE['url'] = "http://www.bunny.cf/"; //网站地址  注意,网址后面必须加上/
-//$SITE['url'] = "http://btsearch-aera.rhcloud.com/"; //网站地址  注意,网址后面必须加上/
-$SITE['title'] = '兔子搜索_磁力种子搜索'; //网站标题
-$SITE['keywords'] = 'BT,BT种子,种子,种子搜索,BT搜索,资源搜'; //网站关键词
-$SITE['description'] = '磁力种子,提供影片搜索、BT种子、视频下载链接在线快速播放'; //网站描述
-$badword = array('胡锦涛','江泽民', '邓小平', '毛泽东');	//过滤的关键词
-$key = 'f9FkTlB25C';	//加盐字符
-$default_keyword = '无人区';  //默认的关键词
+/*
+*   搜索数据备份Beta
+*   将用户搜索过的数据备份到数据库，使用了消息队列来提高速度（需要安装HTTPSQS）
+*/
+// $message_queue = array(
+//     // 是否开启，默认为关闭
+//     'status' => 'FALSE',
+
+//     // HTTPSQS连接配置
+//     'conf' => array(
+//         'host' => '127.0.0.1',     // 监听地址
+//         'port' => '1218',          // 监听端口
+//         'passwd' => 'wszddhhh',    // 认证密钥
+//         'charset' => 'utf-8',      // 编码
+//         )
+// );
+
+/*
+*   分享短网址
+*   默认使用dwz.bt-ss.com接口，自建请发送邮件至kslrwang@gmail.com联系
+*/
+$dwzconf = array(
+    'url' => 'http://www.bunny.cf/',  // 域名(不要忘记‘/’)
+    'skey' => 'W97OY2VmgtG3oJCm',  // 识别密钥
+);
--- php/index.php
+++ php/index.php
@@ -1,225 +1,119 @@
 <?php
-include 'function.php';
+/**
+ * index.php
+ *
+ * 应用程序首页
+ *
+ * @author     Kslr
+ * @copyright  2014 kslrwang@gmail.com
+ * @version    0.3
+ */
 
-if (!empty($_POST['keyword'])) {
-	$search_data_tmp = Get_search(htmlspecialchars(trim($_POST['keyword'])), '', '', $key, $SITE['url']);
-	$search_data = json_decode($search_data_tmp, true);
-	#print_r($search_data['data']);
-	if (!isset($search_data['Error'])) {
-		$keyword = $search_data['keyword'];
-		$collpage = $search_data['collpage'];
-		$currentpage = $search_data['currentpage'];
-		batchsql($search_data['data'], htmlspecialchars(trim($_POST['keyword'])));
-	} else {
-		$keyword = $default_keyword;
-	}
-} elseif (!empty($_GET['keyword'])) {
-	$st = false;
-	$keyword = htmlspecialchars(trim($_GET['keyword']));
+include dirname(__FILE__).'/config.php';
+include APP_ROOT.'/include/core.php'; 
+include APP_ROOT.'/include/template/header.php';
+
+if(isset($_GET['error'])) {
+  $error_code = intval($_GET['error']);
+    switch ($error_code) {
+      case '0':
+        $default_keyword = '此关键词被列入黑名单！';
+        break;
+      case '1':
+        $default_keyword = '使用了错误的页码';
+        break;
+      case '2':
+        $default_keyword = '抱歉，未能搜索到数据。';
+        break;
+      case '3':
+        $default_keyword = '详情页使用了错误的HASH ！';
+        break;
+      default:
+        $default_keyword = $siteconf['default_keyword'];
+        break;
+    }
 } else {
-	$st = true;
-	$keyword = null;
+  $default_keyword = $siteconf['default_keyword'];
 }
 
-
-if (!empty($_GET['keyword']) && !empty($_GET['collpage']) && !empty($_GET['currentpage'])) {
-	$search_data_tmp = Get_search(htmlspecialchars(trim($_GET['keyword'])), htmlspecialchars(trim($_GET['currentpage'])), htmlspecialchars(trim($_GET['collpage'])), $key, $SITE['url']);
-	$search_data = json_decode($search_data_tmp, true);
-	if (!isset($search_data['Error'])) {
-		$keyword = $search_data['keyword'];
-		$collpage = $search_data['collpage'];
-		$currentpage = $search_data['currentpage'];
-	} else {
-		$keyword = $default_keyword;
-	}
-}
-
-// 处理磁力链转换为种子
-if (!empty($_GET['magnetbt'])) {
-	Magnetic_turn_seeds(strip_tags(trim($_GET['magnetbt'])));
-}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?php echo $SITE['title']; ?></title>
-  <meta name="keywords" content=<?php echo $SITE['keywords']; ?> />
-  <meta name="description" content="<?php echo $SITE['description']; ?>" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="public/css/bootstrap.min.css">
-  <link rel="stylesheet" href="public/css/bootstrap-theme.min.css">
-  <link href="public/css/style.css" rel="stylesheet">
-  <script type="text/javascript" src="public/js/jquery.min.js"></script>
-  <script type="text/javascript" src="public/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-  <!-- 网站导航栏 -->
-  <div class="navbar navbar-default" role="navigation">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="/">兔子搜索</a>
-    </div>
-    <div class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="/">首页</a></li>
-        <li><a href="" data-toggle="modal" data-target="#face">反馈建议</a></li>
-        <li><a href="javascript:alert('请给我发送邮件 Mail: jufuli.ml@gmail.com')">联系我们</a></li>
-        <li><a target="_blank" href="http://www.jufuli.ml">福利资源</a></li>
-      </ul>
 
-    </div>
+<div id="keyword">
+  <div class="tags">
+    <?php 
+        foreach(Popular_keywords() as $popularkeyword){
+          echo '<a href="search.php?keyword='.$popularkeyword.'" class="label label-primary tags_a" target="_blank">'.$popularkeyword.'</a> ';
+        }
+    ?>
   </div>
-  <!-- 网站导航栏结束 -->
-  <!-- 顶部提示 -->
-  <!-- <div class="alert alert-info alert-dismissable">
-      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-      <p>不要搜索这么屌的关键词了，小心被和谐。<br>程序开发测试中，偶尔可能会无法使用<br>另外，维护和谐社会，共创美好明天。</p>
-  </div> -->
 
+  <div class="hide_keyword">
+    <p id="hide_keyword">关闭热门关键词</p>
+  </div>
 
-  <div class="row">
-    <div class="col-lg-12 col-lg">
-      <center><img src="public/img/logo.jpeg" width="300px"></img></center>
-      <form class="navbar-form navbar-left" role="search" method="post" action="index.php">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="<?php echo htmlspecialchars($keyword);?>" name="keyword">
-          <input type="hidden" name="key" value="f9FkTlB25C">
-        </div>
-        <button type="submit" class="btn btn-default">搜索</button>
+</div>
+<script type="text/javascript">
+  $("#keyword").hide();
+    $(document).ready(function(){
+      $("#show_keyword").click(function(){
+      $("#keyword").slideDown(500);
+      });
+     $("#hide_keyword").click(function(){
+      $("#keyword").slideUp(500);
+      $("#show_keyword").show();
+     });
+  });
+</script>
+
+<div class="header">
+  <span id="show_keyword" class="glyphicon glyphicon-bookmark"></span>
+</div>
+
+<div id='warp'>
+  <div class="search">
+    <div class="logo">
+      <img src="<?php echo $siteconf['url'].'public/images/logo.jpg'; ?>" />
+    </div>
+
+      <form class="search_form" role="search" method="get" action="search.php">
+      <input name="keyword" class="form-control" autofocus="autofocus"  placeholder="<?php echo $default_keyword; ?>" x-webkit-speech lang='zh-CN' required="required"/>
+      <button class="btn search_btn" aria-label="搜一下" id="search_btn"><span>搜索</span></button>
       </form>
-    </div>      
   </div>
 
-  <!-- 顶部刚搜索的关键词 -->
-  <div class="row">
-    <div class="col-lg-12 col-lg">
-    	<h4>刚刚被搜索的词:</h4>
-    	<?php 
-    	foreach(Recentsearches() as $keyword_cont){
-    		echo '<a href="index.php?keyword='.$keyword_cont['tags'].'" class="label label-primary" target="_blank">'.$keyword_cont['tags'].'</a> ';
-    	} 
-    	?>
-    </div>      
+  <div class="navbar footer navbar-fixed-bottom">
+    <span id="fsr">
+       <span class="_le" >© 2014 BT-SS.com</span>
+     </span>
+
+    <span id="fsl">
+      <a class="_le" data-toggle="modal" data-target="#ad" style="text-decoration:none;">合作/广告/反馈</a>
+      <a class="_le" style="color:rgb(202, 24, 24); margin-left:10px;" target="_blank" href="<?php echo $siteconf['url'].'/m'; ?>">移动网站</a>
+      <a class="_le" style="margin-left:10px;" target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=d1a3001b426c8e2afd7bc6eb4e788349993e719957bcaa8afea8107eb0a5f0eb"><img border="0" src="https://www.bt-ss.com/public/images/group.png" alt="BT-SS用户交流" title="BT-SS用户交流"></a>
+    </span>
   </div>
+</div>
 
-    <!-- 刚刚搜索过的种子列表 -->
-	<div class="history">
-		<div class="col-lg-12">
-                    <h4>刚刚搜索的种子</h4>
-			<?php 
-			// 在线播放API网站列表
-			$Broadcast = array (
-				'电影离线点播' => 'http://yun.dybeta.com/play.php#!u=',
-				'火焰云点播' => 'http://www.huoyan.tv/api.php#!u=',
-				'微点播' => 'http://www.weivod.com/?u=',
-				'多姿云点播' => 'http://www.vodzx.com/#!u=',
-			);
-			// 种子列表表格
-			if (isset($search_data)) {
-				if (isset($search_data['Error'])) {
-					echo "<table class=\"table table-bordered table table-hover\" border=\"1\">";
-					echo "<tr>";
-					echo "<td>".$search_data['Error']."</td>";
-					echo "</tr></table>";
-					$list = RecentBT();
-				} else {
-					$list = $search_data['data'];
-				}
-			} else {
-				$list = RecentBT($st, $keyword);
-			}
-			echo "<table class=\"table table-bordered table table-hover\" border=\"1\"><tr><th id='thdn'>影片名字</th><th>种子大小</th><th>上传日期</th><th>磁力/种子</th><th>在线观看</th></tr>";
-				if (is_array($list)) {
-					foreach ($list as $magnetic ) {
-						echo "<tr>";
-						echo "<td id='list_td'>" . $magnetic['name'] . "</td>";
-						echo "<td id='list_td'>" . $magnetic['size'] . "</td>";
-						#echo "<td id='list_td'>" . date('Y-m-d', strtotime($magnetic['date'])) . "</td>";
-						echo "<td id='list_td'>" . $magnetic['date'] . "</td>";
-						echo "<td id='list_td'><a href='".$magnetic['url']."'>磁力<a><a href='index.php?magnetbt=".$magnetic['url']."'/>种子</a></td>";
-						echo "<td>";
-						echo '<div class="btn-group">
-							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-						    在线观看 <span class="caret"></span>
-						  </button><ul class="dropdown-menu" role="menu">';
-						foreach ($Broadcast as $broad_title => $broad_url ) {
-							echo '<li><a target="_blank" href="'.$broad_url.$magnetic['url'];
-							echo '"> '.$broad_title.'</a></li>';
-						}
-						echo "</ul></div>";
-						echo "</td>";
-						echo "</tr>";
-					}
-				} else {
-					echo '<tr><td>没有数据，请尝试其他关键词</td><td></td><td></td><td></td><td></td></tr>';
-				}
-			echo '</table>';
-			?>
-			</div>
-		</div>
-		<!-- 列表底部页码-->
-		<?php
-		#print_r($search_data['collpage']);
-		if (!empty($search_data['collpage']) && !empty($search_data['currentpage'])) {
-			$collpage = intval($search_data['collpage']);
-			$currentpage = intval($search_data['currentpage']);
-			if ($collpage != '0') {
-				if ($currentpage >= '10') {
-					$currentpage_sta = $currentpage - '4';
-					$currentpage_end = $currentpage + '5';
-				} else {
-					$currentpage_sta = '1';
-					$currentpage_end = '10';
-				}
-				if ($currentpage_end > $collpage) $currentpage_end = $collpage;
-				echo '<ul class="pagination">';
-				for ($i = $currentpage_sta; $i <= $currentpage_end; $i++) {
-					if ($currentpage == $i || $currentpage == '0') {
-						echo '<li class="active"><a href="index.php?keyword='.$keyword.'&collpage='.$collpage.'&currentpage='.$i.'">'.$i.'</a></li>';
-					} else {
-						echo '<li><a href="index.php?keyword='.$keyword.'&collpage='.$collpage.'&currentpage='.$i.'">'.$i.'</a></li>';
-					}
-
-				}
-				echo '</ul>';
-			}
-		}
-		?>
-		<!-- 网站底部页码结束 -->
-	</div>
-	<!-- 网站主体结束 -->
-  
-  
-  <!-- 底部 -->
-  <div class="footer navbar-default">
-  		<p>© 2014 Bunny.CF All Rights Reserved.</p>
-  </div>
-  <!-- 底部结束 -->
-
-<!-- 建议反馈 -->
-<div class="modal fade" id="face" tabindex="-1" role="dialog" aria-labelledby="facev" aria-hidden="true">
+<div class="modal fade" id="ad" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="facev">建议反馈</h4>
-      </div>
       <div class="modal-body">
-        <!-- 多说评论框 -->
-		<div class="ds-thread"></div>
-		<script type="text/javascript">
-          var duoshuoQuery = {short_name:"jfl"};
-        	(function() {
-            var ds = document.createElement('script');
-            ds.type = 'text/javascript';ds.async = true;
-            ds.src = 'http://static.duoshuo.com/embed.js';
-            ds.charset = 'UTF-8';
-            (document.getElementsByTagName('head')[0] 
-            || document.getElementsByTagName('body')[0]).appendChild(ds);
-        	})();
-        </script>
-		<!-- 多说评论框结束 -->
+          <h3>站点合作</h3>
+            若不团结,任何力量都是弱小的，只有进行合作，强强联合，集双方之优势，获取合作进步，才能达到利益双赢的局面。
+          <hr>
+
+          <h3>投放广告</h3>
+            广告位置：
+            <li>搜索结果页上方970*90 ￥80/月</li>
+            <li>详情页728*90 ￥75/月</li><br>
+            流量数据请向我索取，联系方式在最后。
+
+          <hr>
+          
+          <h3>建议反馈</h3>
+            邮件联系：Kslrwang@gmail.com<br>
+            QQ群：207302852
+
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
@@ -227,6 +121,6 @@ if (!empty($_GET['magnetbt'])) {
     </div>
   </div>
 </div>
-<!-- 建议反馈结束 -->
-</body>
-</html>
+<!-- 网站内容结束 -->
+
+<?php include APP_ROOT.'/include/template/footer.php'; ?>

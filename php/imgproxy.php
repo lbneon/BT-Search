d43ab942153cc7813a0e39d0a9ad242459bcf4d8
lbneon <lbneon@gmail.com>
2014-07-07 22:04:59 +0000

    update

---
+++ php/imgproxy.php
@@ -0,0 +1,8 @@
+<?php
+  $img=$_GET['img'];
+  echo file_get_contents($img);
+  //http://www.111cn.net/phper/php-cy/48682.htm
+  //http://www.iippcc.com/ru-he-po-jie-tu-pian-fang-dao-lian.html
+  //http://www.cnblogs.com/hooray/archive/2011/05/12/2044744.html
+  
+?>

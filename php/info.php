f23edc885a2669ecb0c27611a0d8f08a1958da3b
lbneon <lbneon@gmail.com>
2014-08-08 20:07:37 +0000

    update

--- php/info.php
+++ php/info.php
@@ -18,11 +18,11 @@
     exit();
   }
 
-  print($info['title'] . 'aa');
   // 创建短地址
   $dwz = create_dwz($siteconf['url'].'info.php?magnetic='.$_GET['magnetic']);
   print($info['title'] . 'bb');
   $global_title = $info['title'] . 'aa';
+  print($global_title)
   include APP_ROOT.'/include/template/header.php';
 ?>
 <div class="container">

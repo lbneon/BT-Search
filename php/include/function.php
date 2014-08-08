e6e3a0767205cdbe186c7a6f790883aa4ab7a737
lbneon <lbneon@gmail.com>
2014-08-08 15:18:06 +0000

    update

--- php/include/function.php
+++ php/include/function.php
@@ -47,7 +47,7 @@ function Popular_keywords_tk()
             $html->load(curl_exec ( $process ));
             curl_close ( $process );
 
-            print($html);
+            print($html->find('div[id=main]')->find('a',0)->innertext );
             //foreach($html->find('div[id=main]')->find('a') as $name) {
 			//	$content[] = $name->innertext;
 			//}

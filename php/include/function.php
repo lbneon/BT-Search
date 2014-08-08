beae96764e161f128fcdceb5aabf5cac0c4f5c0b
lbneon <lbneon@gmail.com>
2014-08-08 14:34:01 +0000

    update

--- php/include/function.php
+++ php/include/function.php
@@ -47,7 +47,7 @@ function Popular_keywords_tk()
 	        $html = curl_exec ( $process );
 	        curl_close ( $process );
             
-            foreach($html->find('div.wrapper a') as $name) {
+            foreach($html->find('div.wrapper') as $name) {
 				$content[] = $name;
 				print($name);
 			}
